f223a399022938122a12670217528ccae278ea4d
TBleess <todd.bleess@state.co.us>
2021-07-29 10:42:22 -0600

    2020

--- src/common/js/add_title_control.js
+++ src/common/js/add_title_control.js
@@ -9,7 +9,7 @@ module.exports = function(map: Object) {
     });
     title.onAdd = function() {
         var div = L.DomUtil.create('div', 'title bord');
-        div.innerHTML = '<h2>Estimated Job Sectors - 2019</h2>';
+        div.innerHTML = '<h2>Estimated Job Sectors - 2020</h2>';
         return div;
     };
     title.addTo(map);
